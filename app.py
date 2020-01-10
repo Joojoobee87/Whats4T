74667db4d1e3452b8cc6fed364e4836f3bf44c64
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-01-10 13:48:01 +0000

    created new Whats4T project and app.py file

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+    
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get('MONGODB_NAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+                           recipes=mongo.db.recipes.find())
+
+
+@app.route('/create_recipe')
+def create_recipe():
+    return render_template("createrecipe.html",
+                            recipes=mongo.db.recipes())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
