77476a29db1b29dda0b9ae7a32fbf48fde7a1690
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-01-13 21:42:07 +0000

    added form validator to username field for min and max length

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, Email, EqualTo, Length,
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length,
 
 
 class RegistrationForm(Form):
-    username = StringField('Username', validators=DataRequired())
+    username = StringField('Username', validators=DataRequired(), Length(min=6, max=20))
     email = StringField('Email', validators=Email(), DataRequired())
     password = PasswordField('Password', validators=DataRequired(), EqualTo('Confirm Password'))
     confirm_password = PasswordField('Confirm Password', validators=DataRequired())
@@ -13,3 +13,4 @@ class RegistrationForm(Form):
 class LoginForm(Form):
     email = StringField('Email', validators=DataRequired(), Email())
     password = PasswordField('Password', validators=DataRequired())
+
