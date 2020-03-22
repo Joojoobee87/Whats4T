522ef9c034e1a94c8063dc44fd16b13210ff00ac
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2020-03-22 17:35:28 +0000

    removed modules not is use and form fields not utilised

--- forms.py
+++ forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length, ValidationError
-from flask import url_for
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20), ])
     email = StringField('Email', validators=[Email(), DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
@@ -15,5 +14,4 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Email(), DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
